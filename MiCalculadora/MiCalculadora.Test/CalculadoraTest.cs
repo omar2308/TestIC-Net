a16f221c08ade2c173c05cd0e6a5d9671d5bef8d
Omar Fernandez <omarfl7@gmail.com>
2017-10-01 04:34:50 -0500

    Agregando fuentes

---
+++ MiCalculadora/MiCalculadora.Test/CalculadoraTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MiCalculadora.Test
+{
+    [TestClass]
+    public class CalculadoraTest
+    {
+        [TestMethod]
+        public void Sumar4y1_resulta5()
+        {
+            Calculadora calc = new Calculadora();
+            int resultado = calc.sumar(1, 4);
+            Assert.AreEqual(5, resultado);
+        }
+        [TestMethod]
+        public void Sumar7y10_resulta17()
+        {
+            Calculadora calc = new Calculadora();
+            int resultado = calc.sumar(7, 10);
+            Assert.AreEqual(17, resultado);
+        }
+    }
+}
